76b842b1693346e771d5c618d828c081e9547ea2
bokiscout <borchejankovski@gmail.com>
2019-02-06 16:37:01 +0100

    Initial commit for NRF development
    Add SDK, compiler, toolchain, and ADC example as starting point

---
+++ build/envrironment.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# path to arm gcc compiler
+PATH=$PATH:../gcc-arm-none-eabi/bin
+
+# path to mergehex tool
+PATH=$PATH:../nRF-Command-Line-Tools/mergehex
+
+# path to nrfjprog tool
+PATH=$PATH:../nRF-Command-Line-Tools/nrfjprog
